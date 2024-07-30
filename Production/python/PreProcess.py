ae53604e7ae16d8b8d8d6ab00ad59f6dd007ae0b
lucasrussell01 <lucascharlesrussell@gmail.com>
2024-07-30 15:47:04 +0100

    update production paths and vars

--- Production/python/PreProcess.py
+++ Production/python/PreProcess.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 
 # Select same sign of opposite sign pairs from parquet files
 
-base_dir = '/vols/cms/lcr119/offline/HiggsCP/data/raw/2022/tt'
+base_dir = '/vols/cms/lcr119/offline/HiggsCP/HiggsDNA/output/testxx/Run3_2022/tt'
 
 samples = {
     'DYto2L_M-50_madgraphMLM': {'data': False, 'label': 0, 'x_sec': 6282.6, 'n_eff': 145286646, 'process': 'DY'},
@@ -32,7 +32,7 @@ def proc_weight(x_sec, n_eff, lumi = 8077):
 
 # Features to keep
 features = ['pt_1', 'pt_2', 'eta_1', 'eta_2', 'phi_1', 'phi_2', 'dR', 'pt_tt', 'pt_tt_met',
-            'mt_1', 'mt_2', 'mt_lep', 'mt_tot', 'met', 'met_phi', 'met_dphi_1', 'met_dphi_2', 
+            'mt_1', 'mt_2', 'mt_lep', 'mt_tot', 'met_pt', 'met_phi', 'met_dphi_1', 'met_dphi_2',
             'dphi', 'm_vis', 'pt_vis', 'n_jets', 'n_bjets', 'mjj', 'jdeta', 'sjdphi', 'dijetpt', 
             'jpt_1', 'jpt_2', 'jeta_1', 'jeta_2', 'jphi_1', 'jphi_2', 
             'weight', 'class_label']
