8ed47eb9f2246b42f10a8ebe2694ff7a0a1da9aa
lucasrussell01 <lucascharlesrussell@gmail.com>
2024-09-23 12:14:37 +0100

    Update hyperparams + AMS on validation

--- Training/python/train_BDT.py
+++ Training/python/train_BDT.py
@@ -4,15 +4,51 @@ from sklearn.metrics import accuracy_score
 import pandas as pd
 import os
 import yaml
+import numpy as np
+from statsmodels.stats.weightstats import DescrStatsW
 
-
-def load_ds(path, feat_names, y_name, w_name):
+def load_ds(path, feat_names, y_name, w_name, eval = False):
     df = pd.read_parquet(path)
     x = df[feat_names]
     y = df[y_name].replace({11: 1, 12: 1})
     w = df[w_name]
-    return x, y, w
+    if eval:
+        phys_w = df['weight']
+        return x, y, w, phys_w
+    else:
+        return x, y, w
 
+def AMS(S, B, b0=0):
+    ams = np.sqrt(2*((S+B+b0)*np.log(1+S/(B+b0))-S))
+    return ams
+
+def val_eval(model, cfg):
+    val_path = os.path.join(cfg['Setup']['input_path'], 'ShuffleMerge_VAL.parquet')
+    x, y, w_NN, w_phys = load_ds(val_path, cfg['Features']['train'],
+                                 cfg['Features']['truth'], cfg['Features']['weight'], eval=True)
+    # Get predictions
+    y_pred_proba = model.predict_proba(x) # raw score
+    y_pred = y_pred_proba.argmax(axis=1) # predicted label
+    # Accuracy
+    accuracy = accuracy_score(y, y_pred, sample_weight=w_NN)
+    print("Validation Accuracy:", accuracy)
+    # Find events classified as Higgs
+    y_pred_higgs = y_pred_proba[:, 1][y_pred==1]   # get raw Higgs scores of events classified as Higgs
+    w_pred_higgs = w_phys[y_pred==1]          # get weights for those events
+    y_higgs = y[y_pred==1]                # truth for Higgs classified events
+    # Optimised binning (flat signal)
+    n_bins = 5
+    w_perc = DescrStatsW(y_pred_higgs[(y_higgs == 1)], weights=w_pred_higgs[(y_higgs == 1)]).quantile(np.linspace(0, 1, n_bins+1)[1:-1]) # percentiles
+    bins = np.concatenate([[0.33], np.array(w_perc), [1]])
+    # Histogram signal and background
+    y_higgs = y[y_pred==1]                # truth for Higgs classified events
+    s_counts = np.histogram(y_pred_higgs[(y_higgs == 1)], bins=bins, weights=w_pred_higgs[(y_higgs == 1)])[0]
+    bkg_counts = np.histogram(y_pred_higgs[(y_higgs != 1)], bins=bins, weights=w_pred_higgs[(y_higgs != 1)])[0]
+    # AMS Score
+    ams = AMS(s_counts, bkg_counts)
+    print("AMS Score (bin by bin):", ams)
+    print(f"Overall AMS: {np.sqrt(np.sum(ams**2))}")
+    del x, y, w_NN, w_phys
 
 def train_model(cfg):
     # Input path
@@ -23,9 +59,8 @@ def train_model(cfg):
                                         cfg['Features']['truth'], cfg['Features']['weight'])
 
     # Model training
-    print("Training model")
-    model = XGBClassifier(objective='multi:softmax', num_class=3, learning_rate=0.05,
-                            n_estimators=750, max_depth=3, min_child_weight=1, reg_lambda=1)
+    print("Training XGBClassifier model")
+    model = XGBClassifier(**cfg['param'])
     model.fit(x_train, y_train, sample_weight=w_train)
 
     # Save model
@@ -36,7 +71,8 @@ def train_model(cfg):
     model.save_model(save_path)
     print(f"Training Complete! Model saved to: {save_path} \n")
     # Get Training accuracy
-    accuracy = predict_acc(model, x_train, y_train, w_train)
+    y_pred_labels = model.predict(x_train)
+    accuracy = accuracy_score(y_train, y_pred_labels, sample_weight=w_train)
     print("Training Accuracy:", accuracy)
     # Save features used:
     with open(os.path.join(save_dir, 'train_cfg.yaml'), 'w') as f:
@@ -45,26 +81,12 @@ def train_model(cfg):
 
     return model
 
-def predict_acc(model, x, y, w):
-    y_pred = model.predict_proba(x)
-    y_pred_labels = y_pred.argmax(axis=1)
-    accuracy = accuracy_score(y, y_pred_labels, sample_weight=w)
-    return accuracy
-
-def val_acc(model, cfg):
-    val_path = os.path.join(cfg['Setup']['input_path'], 'ShuffleMerge_VAL.parquet')
-    x_val, y_val, w_val = load_ds(val_path, cfg['Features']['train'],
-                                        cfg['Features']['truth'], cfg['Features']['weight'])
-    accuracy_val = predict_acc(model, x_val, y_val, w_val)
-    print("Validation Accuracy:", accuracy_val)
-    del x_val, y_val, w_val
-
 
 
 def main():
     cfg = yaml.safe_load(open("../config/BDTconfig.yaml"))
     model = train_model(cfg)
-    val_acc(model, cfg)
+    val_eval(model, cfg)
 
 if __name__ == "__main__":
     main()
